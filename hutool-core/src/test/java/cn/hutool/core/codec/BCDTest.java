b07c4e102c8e4dc1b61ccee2e469b9bf0a9ffdc8
yuanjun <eugenelau@foxmail.com>
2024-08-13 22:23:18 +0800

    decp BCDTest

--- hutool-core/src/test/java/cn/hutool/core/codec/BCDTest.java
+++ hutool-core/src/test/java/cn/hutool/core/codec/BCDTest.java
@@ -3,12 +3,16 @@ package cn.hutool.core.codec;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 当前的这个实现已经被废弃
+ */
 public class BCDTest {
 
 	@Test
 	public void bcdTest(){
 		final String strForTest = "123456ABCDEF";
 
+		// 什么是BCD？BCD码这种编码形式利用了四个位元来储存一个十进制的数码，使二进制和十进制之间的转换得以快捷的进行
 		//转BCD
 		final byte[] bcd = BCD.strToBcd(strForTest);
 		final String str = BCD.bcdToStr(bcd);
